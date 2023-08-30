2acc77accd23080b150e973ef4b2bd86764259f0
Konstantin_Ermolaev <kostyaermol@mail.ru>
2023-08-30 15:00:57 +0700

    Initial commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function __construct()
+    {
+
+    }
+
+    public function registerPage()
+    {
+        return view('welcome');
+    }
+
+    public function register()
+    {
+        
+    }
+}
