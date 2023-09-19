1fa2b8b2795ee2e0742d71e62eb214ae0555b4e4
Konstantin_Ermolaev <kostyaermol@mail.ru>
2023-09-19 13:16:34 +0700

    Добавил поля к формам
    Добавил поля к формам регистрации и входа.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,6 +2,10 @@
 @section('title', 'Вход')
 @section('content')
     <form action="{{route('auth')}}" method="post">
-
+        <label for="email">Логин:</label>
+        <input type="email" name="email">
+        <br>
+        <label for="password">Пароль:</label>
+        <input type="password" name="password">
     </form>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,6 +2,13 @@
 @section('title', 'Регистрация')
 @section('content')
     <form action="{{route('user.store')}}" method="post">
-
+        <label for="name">Введите имя:</label>
+        <input type="text" name="name" placeholder="Введите имя">
+        <br>
+        <label for="email">Введите email:</label>
+        <input type="email" name="email" placeholder="email@mail.ru">
+        <br>
+        <label for="password">Введите пароль:</label>
+        <input type="password" name="password" >
     </form>
 @endsection
